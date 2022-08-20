28a356471133ce369b4c674e7fab0e5734410091
Шаталов Роман <shatalovrs@it2g.ru>
2022-08-20 20:53:29 +0500

    - хук сам дописывает в `style` параметр стиля `transition`, опираясь на входящий `delay`

--- use-animate.ts
+++ use-animate.ts
@@ -1,28 +1,34 @@
 import { CSSProperties, useCallback, useEffect, useState } from "react";
 import {UseAnimateProps} from "./use-animate.model";
 
-
 export const useAnimate = (config: UseAnimateProps) => {
-  const {mount, unMount} = config;
-  const [style, setStyle] = useState<CSSProperties>(unMount.style);
+    const { mount, unMount } = config;
+    const [style, setStyle] = useState<CSSProperties>(unMount.style);
 
-  /** un-mount */
-  const handleClose = useCallback(
-      (handleClick: () => void) => {
-        setStyle(unMount.style);
+    /** un-mount */
+    const handleClose = useCallback(
+        (handleClick: () => void) => {
+            setStyle({
+                ...unMount.style,
+                transition: `all ${unMount.delay / 1000}s`,
+            });
 
-        let timer = setTimeout(() => handleClick(), unMount.delay);
+            let timer = setTimeout(() => handleClick(), unMount.delay);
+            return () => clearTimeout(timer);
+        },
+        [unMount]
+    );
+
+    /** mount */
+    useEffect(() => {
+        let timer = setTimeout(
+            () =>
+                setStyle({ ...mount.style, transition: `all ${mount.delay / 1000}s` }),
+            mount.delay
+        );
+
         return () => clearTimeout(timer);
-      },
-      [unMount]
-  );
+    }, []);
 
-  /** mount */
-  useEffect(() => {
-    let timer = setTimeout(() => setStyle(mount.style), mount.delay);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return {handleClose, style};
+    return { handleClose, style };
 };
